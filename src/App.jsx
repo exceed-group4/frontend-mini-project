da574f97b37883997e6fd41b411811e027bb9c71
panitnt <panittatanya@gmail.com>
2023-02-08 12:01:40 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import './App.css'
+import Room from './component/Room';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Smart Home Lighting</h1>
+      <Room/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Room.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Room = () => {
+    return (
+        <div className='room-setting'>
+            
+        </div>
+    )
+}
+
+export default Room
