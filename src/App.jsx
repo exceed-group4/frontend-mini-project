67321c8327221bf32949e0b0207097542a81f3c4
panitnt <panittatanya@gmail.com>
2023-02-08 13:11:49 +0700

    Change style

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <h1>Smart Home Lighting</h1>
-      <Room/>
+      <Room name='bedroom'/>
     </div>
   );
 }
--- src/component/Room.jsx
+++ src/component/Room.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+// import { getRooms } from '../services/read'
 
-const Room = () => {
+
+const Room = ({name}) => {
+    const [room, setRoom] = useState(true)
+
+    const toogleIsLoading = () => {
+        setRoom(current => (!current))
+        console.log(room)
+    }
+
     return (
         <div className='room-setting'>
+            <h2>{name}</h2>
+            <div>
+            <RadioFormField name="State:" groupName="mode" choices={["auto", "manual"]} onChange={(e) => {setRoom(e.target.value)
+            console.log(e.target.value)}} />
+            </div>
+            <label class="switch">
+                <input type="checkbox" value='' onChange={toogleIsLoading}/>
+                <span class="slider round"></span>
+            </label>
+
             
         </div>
     )
 }
 
 export default Room
+
+
+const RadioFormField = ({ name, choices, onChange, groupName }) => {
+	return (
+		<div className="form-field" onChange={onChange}>
+			<label>{name}</label>
+			{choices.map(c => <div className="radio-field"><input type="radio" value={c} name={groupName} />{c}</div>)}
+		</div>
+	)
+}
